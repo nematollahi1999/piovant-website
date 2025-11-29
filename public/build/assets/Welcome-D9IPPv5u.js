f35ea2147de752b389c4ea141a83a0751e929ee3
nematollahiLaptop <nematollahi.1999@gmail.com>
2025-11-29 11:25:11 +0330

    Add Vite production build

---
+++ public/build/assets/Welcome-D9IPPv5u.js
@@ -0,0 +1,1 @@
+import{i as e,c as o,o as t}from"./app-CuamL9bJ.js";/* empty css            */const l=e({__name:"Welcome",setup(c){return(n,r)=>(t(),o("div",null,"Welcome"))}});export{l as default};
